356e33449af5c0dc4a3b19f04ed254e69c5985d3
Taehee Lee <kairess87@gmail.com>
2019-05-28 19:29:17 +0900

    add glados dataset

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@ import argparse
 import os
 from multiprocessing import cpu_count
 from tqdm import tqdm
-from datasets import blizzard, ljspeech
+from datasets import blizzard, ljspeech, glados
 from hparams import hparams
 
 
@@ -21,6 +21,13 @@ def preprocess_ljspeech(args):
   metadata = ljspeech.build_from_path(in_dir, out_dir, args.num_workers, tqdm=tqdm)
   write_metadata(metadata, out_dir)
 
+def preprocess_glados(args):
+  in_dir = os.path.join(args.base_dir, 'glados_voices')
+  out_dir = os.path.join(args.base_dir, args.output)
+  os.makedirs(out_dir, exist_ok=True)
+  metadata = glados.build_from_path(in_dir, out_dir, args.num_workers, tqdm=tqdm)
+  write_metadata(metadata, out_dir)
+
 
 def write_metadata(metadata, out_dir):
   with open(os.path.join(out_dir, 'train.txt'), 'w', encoding='utf-8') as f:
@@ -35,15 +42,17 @@ def write_metadata(metadata, out_dir):
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--base_dir', default=os.path.expanduser('~/tacotron'))
+  parser.add_argument('--base_dir', default=os.path.expanduser('~/Documents/etc_dataset'))
   parser.add_argument('--output', default='training')
-  parser.add_argument('--dataset', required=True, choices=['blizzard', 'ljspeech'])
+  parser.add_argument('--dataset', required=True, choices=['blizzard', 'ljspeech', 'glados'])
   parser.add_argument('--num_workers', type=int, default=cpu_count())
   args = parser.parse_args()
   if args.dataset == 'blizzard':
     preprocess_blizzard(args)
   elif args.dataset == 'ljspeech':
     preprocess_ljspeech(args)
+  elif args.dataset == 'glados':
+    preprocess_glados(args)
 
 
 if __name__ == "__main__":
